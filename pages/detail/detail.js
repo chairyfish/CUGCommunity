028b865a00b01fedaf316388dd3ab553da6c0cd0
chairyfish <hlmahz@163.com>
2017-08-06 14:50:07 +0800

    增加了下拉刷新和上拉加载

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -2,7 +2,9 @@
 Page({
   data:{},
   onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+    this.setData({
+      question: options.question
+    })
   },
   onReady:function(){
     // 页面渲染完成
--- pages/home/home.js
+++ pages/home/home.js
@@ -6,13 +6,20 @@ Page( {
     /** 页面配置 */  
     winWidth: 0,  
     winHeight: 0,  
+
     /* tab切换  */
     currentTab: 0,  
 
+    sstatus: 1,// 1是滑动到顶部 2是滑动中 3是滑动到底部
+    isRefresh: false,//是否显示刷新头
+    isLoadMore: false,//加载更多
+    clientY: 0,//Y方向手指按下的方向
+
+    // 卡片
     card:{
       imgpath:"/images/icon/测试头像.jpg",
       nickname:"谣言传播者",
-      time:"2017-07-12 17:00:00",
+      time:"3天前",
       question:"有什么问题是一顿火锅不能解决的？",
       answer:"当然是...没有辣椒没有醋没有麻酱没有蒜泥没有香菜啦",
       detail:"3评论 · 6赞同"
@@ -46,9 +53,7 @@ Page( {
    * 点击tab切换 
    */  
   swichNav: function( e ) {  
-  
     var that = this;  
-  
     if( this.data.currentTab === e.target.dataset.current ) {  
       return false;  
     } else {  
@@ -58,14 +63,13 @@ Page( {
     }  
   } ,
 
-/**  canvas绘制底部阴影  **/
+  /**  canvas绘制底部阴影  **/
   canvasIdErrorCallback: function (e) {
     console.error(e.detail.errMsg)
   },
   onShow: function (e) {
     // 使用 wx.createContext 获取绘图上下文 context
     var context = wx.createCanvasContext('firstCanvas')
-
     // Create linear gradient
     const grd = context.createLinearGradient(0, 0, 0, 80)
     grd.addColorStop(0, 'rgba(255, 255, 255, 0)')
@@ -77,6 +81,8 @@ Page( {
     context.draw()
   },
 
+
+  /* 页面跳转 */
   junpToSearch:function(e) {
     wx.navigateTo({
       url: '../search/search'//路径
@@ -85,10 +91,106 @@ Page( {
   jumpToDetail:function(e) {
     console.log("redirect")
     wx.navigateTo({
-      url: '../detail/detail'//路径
+      url: '../detail/detail?question=666'//路径
     })
+  },
+
+
+
+
+/* 上拉加载与下拉刷新 */
+
+
+  //滑动到顶端
+  scrollToTop: function (e) {
+     this.setData({
+        sstatus: 1
+     })
+  },
+  //滑动到底部
+  scrollToBottom: function (e) {
+     this.setData({
+        sstatus: 3
+    })
+  },
+  //  滑动中
+  scroll: function (e) {
+     this.setData({
+        sstatus: 2,
+     })
+
+  },
+
+  //手指按下监听
+  start: function (e) {
+        console.log(e)
+        var touchPoint = e.touches[0];
+        var clientY = touchPoint.clientY
+        this.setData({
+            clientY: clientY
+        })
+  },
+  //手指抬起监听
+  end: function (e) {
+        console.log(e)
+        var context = this
+        var upPoint = e.changedTouches[0];
+        var endY = upPoint.clientY
+        var pointTopointY = Math.abs(endY - this.data.clientY)
+        var status = this.data.sstatus
+        // wx.showToast({
+        //   title: "滑动的距离:" + pointTopointY + "----:当前的状态:" + status,
+        //   icon: 'loading'
+        // });
+        
+        //上拉刷新
+        if (status == 1 && pointTopointY > 50) {
+            this.setData({
+                isRefresh: true
+            })
+        }
+        //上拉加载
+        if (status == 3 && pointTopointY > 50) {
+            this.setData({
+                isLoadMore: true
+            })
+             wx.showToast({
+              title: '加载更多',
+              icon: 'loading'
+            });
+        }
+        //两秒后隐藏加载条条
+        setTimeout(function () {
+            context.setData({
+                isRefresh: false,//是否显示刷新头
+                isLoadMore: false,//加载更多
+            })
+        }, 3000)
+    },
+
+
+  refresh: function(e){
+    wx.showToast({
+      title: '下拉刷新',
+      icon: 'loading'
+    });
+    
+  },
+
+  onShareAppMessage: function () {
+　　// 微信分享需要的配置参数
+　　return {
+　　title: '地大人社区',
+　　desc: '今日热门的回答都在这儿了',
+　　path: '/page/home/home'
+　　}
   }
 })  
 
-
+function loadMore(){
+  wx.showToast({
+        title: '加载更多',
+        icon: 'loading'
+      });
+}
 
